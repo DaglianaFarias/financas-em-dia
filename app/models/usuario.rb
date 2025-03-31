08684863e7912591e78ba8992c3116f990104d69
Dagliana Escauzer de Farias <dagliana.farias@sefin.ro.gov.br>
2025-03-31 12:53:16 -0400

    WIP - visão geral

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -2,4 +2,17 @@ class Usuario < ApplicationRecord
   belongs_to :unidade_familiar
   has_many :receitas
   has_many :forma_pagamentos
+
+  enum :status, { ativo: 1, inativo: 2 }
+  enum :perfil, { administrador: 1, usuario_regular: 2 }
+
+  before_validation :set_valor_padrao, on: :create
+  
+  private
+
+  def set_valor_padrao
+    self.perfil ||= "usuario_regular"
+    self.status ||= "ativo"
+    self.receber_alerta = true if self.receber_alerta.nil?
+  end
 end
