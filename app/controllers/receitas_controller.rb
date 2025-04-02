0f8f9d4ee2c8b0e269d66376beb8ed0dee1e5462
Dagliana Escauzer de Farias <dagliana.farias@sefin.ro.gov.br>
2025-04-02 14:02:02 -0400

    Receitas: index

--- app/controllers/receitas_controller.rb
+++ app/controllers/receitas_controller.rb
@@ -1,25 +1,20 @@
 class ReceitasController < ApplicationController
   before_action :set_receita, only: %i[ show edit update destroy ]
 
-  # GET /receitas or /receitas.json
   def index
     @receitas = Receita.all
   end
 
-  # GET /receitas/1 or /receitas/1.json
   def show
   end
 
-  # GET /receitas/new
   def new
     @receita = Receita.new
   end
 
-  # GET /receitas/1/edit
   def edit
   end
 
-  # POST /receitas or /receitas.json
   def create
     @receita = Receita.new(receita_params)
 
@@ -34,7 +29,6 @@ class ReceitasController < ApplicationController
     end
   end
 
-  # PATCH/PUT /receitas/1 or /receitas/1.json
   def update
     respond_to do |format|
       if @receita.update(receita_params)
@@ -47,7 +41,6 @@ class ReceitasController < ApplicationController
     end
   end
 
-  # DELETE /receitas/1 or /receitas/1.json
   def destroy
     @receita.destroy!
 
@@ -58,12 +51,10 @@ class ReceitasController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_receita
       @receita = Receita.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def receita_params
       params.require(:receita).permit(:usuario_id, :valor, :descricao, :dataEntrada, :dataEncerramento)
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def formatar_data(data, formato: :padrao)
+    return '' if data.blank?
+
+    case formato
+    when :padrao
+      data.strftime("%d/%m/%Y") # Exemplo: 02/04/2025
+    when :completo
+      data.strftime("%d de %B de %Y") # Exemplo: 2 de abril de 2025
+    when :curto
+      data.strftime("%d/%m") # Exemplo: 02/04
+    when :data_hora
+      data.strftime("%d/%m/%Y %H:%M") # Exemplo: 02/04/2025 14:30
+    else
+      data.to_s
+    end
+  end
 end
