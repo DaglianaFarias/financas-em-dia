cb1e435d71897982a6f43fcb96d043b18b576647
Dagliana Escauzer de Farias <dagliana.farias@sefin.ro.gov.br>
2025-04-25 08:38:39 -0400

    Agrupando options de forma de pagamentos por usuarios

--- app/helpers/forma_pagamentos_helper.rb
+++ app/helpers/forma_pagamentos_helper.rb
@@ -1,16 +1,15 @@
 module FormaPagamentosHelper
 
   def formas_de_pagamento
-    usuarios = @unidade_familiar.usuarios.includes(:forma_pagamentos)
-  
-    meios_de_pagamento = []
-    usuarios.each do |usuario|
-      usuario.forma_pagamentos.each do |forma_pagamento|
-        meios_de_pagamento << [ "#{forma_pagamento.nome} | #{t("attributes.forma_pagamento_tipos.#{forma_pagamento.tipo}")}", forma_pagamento.id ]
+    @unidade_familiar.usuarios.includes(:forma_pagamentos).each_with_object({}) do |usuario, formas_de_pagamento_por_usuario|
+      opcoes = usuario.forma_pagamentos.map do |forma_pagamento|
+        [
+          "#{forma_pagamento.nome} | #{I18n.t("attributes.forma_pagamento_tipos.#{forma_pagamento.tipo}")}",
+          forma_pagamento.id
+        ]
       end
+      formas_de_pagamento_por_usuario[usuario.nome] = opcoes
     end
-  
-    meios_de_pagamento
   end
 
   def formatar_nome_e_tipo_pagamento(forma_pagamento)
